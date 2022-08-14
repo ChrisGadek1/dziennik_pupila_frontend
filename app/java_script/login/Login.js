77300183af8c488ae62e53ac5aba913d6debf015
AgaStach <agstach15@gmail.com>
2022-08-14 17:13:08 +0200

    changed background of login screen

--- app/java_script/login/Login.js
+++ app/java_script/login/Login.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import {Text} from "react-native";
+import {SafeAreaView, Text} from "react-native";
 
 const Login = () =>{
     return(
-        <Text>To jest component Login !</Text>
+        <SafeAreaView style={styles.main}>
+            <Text>To jest component Login !</Text>
+        </SafeAreaView>
     )
 }
 
+const styles = {
+    main:{
+        backgroundColor: "#0E252B",
+        height:'100%'
+    }
+}
 export default Login
