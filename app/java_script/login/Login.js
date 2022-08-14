e65e7908687b868e377544fdd265b9bfe500049f
AgaStach <agstach15@gmail.com>
2022-08-14 22:06:29 +0200

    add photo with cat

--- app/java_script/login/Login.js
+++ app/java_script/login/Login.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {SafeAreaView, Text} from "react-native";
+import {Image, SafeAreaView, Text} from "react-native";
 
 const Login = () =>{
     return(
         <SafeAreaView style={styles.main}>
-            <Text>To jest component Login !</Text>
+            <Image style={styles.image} source={require('../../images/cat_login.jpg')}></Image>
         </SafeAreaView>
     )
 }
@@ -13,6 +13,12 @@ const styles = {
     main:{
         backgroundColor: "#0E252B",
         height:'100%'
+    },
+    image:{
+        marginTop: 30,
+        marginLeft: 10,
+        marginRight: 10,
+
     }
 }
 export default Login
